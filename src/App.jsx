8c04c140e5146532ca5fd6b2b8998463a444ad46
luckyantony <83889059+luckyantony@users.noreply.github.com>
2025-04-22 11:51:22 +0300

    Making the app responsive (#11)

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
 import React, { useState } from 'react';
 import SearchBar from './components/SearchBar';
+import ResultDisplay from './components/ResultDisplay';
+import useDictionary from './hooks/UseDictionary';
 
 const App = () => {
   const [searchResult, setSearchResult] = useState('');
 
+  
+  const { data, loading, error } = useDictionary(searchResult);
+
   const handleSearch = (searchTerm) => {
     setSearchResult(searchTerm);
-    console.log('Search Term:', searchTerm); // For now, log it to the console
+    console.log('Search Term:', searchTerm);
   };
 
   return (
     <div>
-      <h1>Search App</h1>
+      <h1>Dictionary App</h1>
       <SearchBar onSearch={handleSearch} />
-      <div>
-        {searchResult && <p>Searching for: {searchResult}</p>}
-      </div>
+
+      <ResultDisplay data={data?.[0]} loading={loading} error={error} />
+
+      {searchResult && <p>Searching for: {searchResult}</p>}
     </div>
   );
 };
