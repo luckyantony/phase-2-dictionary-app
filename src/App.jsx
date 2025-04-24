dbcab7a1467e6a1911f7f99a6055f3c2830c04e2
luckyantony <83889059+luckyantony@users.noreply.github.com>
2025-04-24 07:13:26 +0300

    Feature/luckyantony responsiveness (#13)
    * Making the app responsive
    
    * Styling and making the website responsive

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,26 @@ import useDictionary from './hooks/UseDictionary';
 
 const App = () => {
   const [searchResult, setSearchResult] = useState('');
+  const { data, loading, error } = useDictionary(searchResult);
 
   
   const { data, loading, error } = useDictionary(searchResult);
 
   const handleSearch = (searchTerm) => {
     setSearchResult(searchTerm);
+
+  };
+
+  return (
+    <div className="app-container">
+      <h1 className="title">React Dictionary App</h1>
+      <SearchBar onSearch={handleSearch} />
+      <ResultDisplay 
+        data={Array.isArray(data) ? data[0] : null} 
+        loading={loading} 
+        error={error} 
+      />
+
     console.log('Search Term:', searchTerm);
   };
 
@@ -22,8 +36,10 @@ const App = () => {
       <ResultDisplay data={data?.[0]} loading={loading} error={error} />
 
       {searchResult && <p>Searching for: {searchResult}</p>}
+
     </div>
   );
 };
+
 
 export default App;
